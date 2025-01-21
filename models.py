e07ac9127b793492956497ffd066da2d5ecf510e
Luis dev <duduhora7@gmail.com>
2025-01-21 19:35:31 -0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+    
+    def __init__(self, name, description=None):
+        # Aqui está a indentação correta
+        self.name = name
+        self.description = description
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
